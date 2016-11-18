d37751abee212633020ebca1c4fa21d78cb60645
lksgemin <lucasgemin@gmail.com>
2016-11-18 17:03:42 -0200

    adicionado angular-material

--- mean/exemplo-mean/app.js
+++ mean/exemplo-mean/app.js
@@ -21,11 +21,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/angular', express.static(__dirname + '/node_modules/angular/'));
+app.use('/angular-animate', express.static(__dirname + '/node_modules/angular-animate/'));
+app.use('/angular-aria', express.static(__dirname + '/node_modules/angular-aria/'));
+app.use('/angular-material', express.static(__dirname + '/node_modules/angular-material/'));
 
 app.use('/', index);
 app.use('/users', users);
-app.post('/contato/add', index.adicionaContato);
+app.post('/contato/save', index.adicionaContato);
 app.post('/contato/remove', index.removeContato);
+app.post('/contato/find', index.buscaContatoPeloNome);
 app.get('/contatos', index.listaContatos);
 
 // catch 404 and forward to error handler
